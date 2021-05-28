42039daab87e0ab5218b80f8d7664564e68932c8
Tahir Ahmed <53032124+ttahm3d@users.noreply.github.com>
2021-05-28 21:43:08 +0530

    feat: created basic folder structire

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1> React Starter template</h1>
+      <ul>
+        <li>Typescript</li>
+        <li>Webpack 5</li>
+        <li>SASS</li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
