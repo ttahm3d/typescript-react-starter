dc15f271ba711fd184fa402553bf028406d11c80
Tahir Ahmed <53032124+ttahm3d@users.noreply.github.com>
2021-05-31 10:50:28 +0530

    feat: configured eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	trailingComma: "es5",
+	useTabs: true,
+	tabWidth: 2,
+	semi: true,
+	singleQuote: false,
+	endOfLine: "lf",
+};
--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import "./styles.css";
 import logo from "../public/assets/react-icon.png";
 
 const App = () => {
-  return (
-    <div className="app">
-      <h1>React Starter template</h1>
-      <img src={logo} alt="React Logo" width="400" height="300" />
-      <ul>
-        <li>Typescript</li>
-        <li>Webpack 5</li>
-        <li>SASS</li>
-      </ul>
-    </div>
-  );
+	return (
+		<div className="app">
+			<h1>React Starter template</h1>
+			<img src={logo} alt="React Logo" width="400" height="300" />
+			<ul>
+				<li>Typescript</li>
+				<li>Webpack 5</li>
+				<li>SASS</li>
+			</ul>
+		</div>
+	);
 };
 
 export default App;
