a3f788d7b5a22fd1fd8b8d3c03eb6aed093be1da
Tahir Ahmed <53032124+ttahm3d@users.noreply.github.com>
2021-05-31 11:26:17 +0530

    feat: added sass loader

--- webpack.common.js
+++ webpack.common.js
@@ -2,42 +2,42 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.tsx"),
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader",
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif|ico)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(svg|ttf|eot|ttf|woff(2)?)$/i,
-        type: "asset/inline",
-      },
-    ],
-  },
-  output: {
-    path: path.resolve(__dirname, "./build"),
-    filename: "bundle.js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./public/index.html"),
-    }),
-  ],
+	entry: path.resolve(__dirname, "./src/index.tsx"),
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(ts|js)x?$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "babel-loader",
+					},
+				],
+			},
+			{
+				test: /\.(css|scss)$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(jpg|jpeg|png|gif|ico)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.(svg|ttf|eot|ttf|woff(2)?)$/i,
+				type: "asset/inline",
+			},
+		],
+	},
+	output: {
+		path: path.resolve(__dirname, "./build"),
+		filename: "bundle.js",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "./public/index.html"),
+		}),
+	],
 };
