20dfecc15f381f71ba858123f1e0f12d6bc17322
ttahm3d <ttahm3d@gmail.com>
2021-11-12 14:13:37 +0530

    working on integrating jest and rtl

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import "./styles.css";
-import logo from "../public/assets/react-icon.png";
+// import "./styles.css";
+// import logo from "../public/assets/react-icon.png";
 
-const App = () => {
+const App: React.FC = () => {
 	return (
 		<div className="app">
 			<h1>React Starter template</h1>
-			<img src={logo} alt="React Logo" width="400" height="300" />
+			{/* <img src={logo} alt="React Logo" width="400" height="300" />
 			<ul>
 				<li>Typescript</li>
 				<li>Webpack 5</li>
 				<li>SASS</li>
-			</ul>
+			</ul> */}
 		</div>
 	);
 };
---
+++ src/components/sum.test.ts
@@ -0,0 +1,7 @@
+
+import {sum} from "./sum"
+
+it("sum of 2 and 2 is 4", () => {
+	const c = sum(2, 2);
+	expect(c).toBe(4);
+});
